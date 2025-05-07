0f2d1c5f72ca03df00126fdc82e56b7768dcb305
Ari Suradipraja <ari.suradipraja@ish.co.id>
2025-05-07 15:59:25 +0700

    Update menu.ts

--- src/data/menu.ts
+++ src/data/menu.ts
@@ -4,15 +4,15 @@ export const headerMenu = [
     { name: 'RFC 2350', link: '/theme-info' },
     { name: 'About', link: '/team' },
     { name: 'Blog', link: '/blog' },
-    { name: 'Style-Guide', link: '/style-guide', showArrow: false,
-        children: [
-            { name: 'Typography', link: '/style-guide#typography' },
-            { name: 'Colors', link: '/style-guide#colors' },
-            { name: 'Links', link: '/style-guide#links' },
-            { name: 'Buttons', link: '/style-guide#buttons' },
-            { name: 'Forms', link: '/style-guide#forms' },
-            { name: 'Lists', link: '/style-guide#lists' },
-        ]
+    // // { name: 'Style-Guide', link: '/style-guide', showArrow: false,
+    //     children: [
+    //         { name: 'Typography', link: '/style-guide#typography' },
+    //         { name: 'Colors', link: '/style-guide#colors' },
+    //         { name: 'Links', link: '/style-guide#links' },
+    //         { name: 'Buttons', link: '/style-guide#buttons' },
+    //         { name: 'Forms', link: '/style-guide#forms' },
+    //         { name: 'Lists', link: '/style-guide#lists' },
+    //     ]
     }
 ];
 
